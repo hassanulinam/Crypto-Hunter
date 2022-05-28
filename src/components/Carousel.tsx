8f9123784201611403bf8d1d3532387f76b607d5
hassanulinam <hassanulinam143@gmail.com>
2022-05-28 19:49:59 +0530

    completed fetching Trending-coins, and desplaying coin-items in Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../config/api";
+import { CryptoState } from "../context/CryptoContextProvider";
+const useStyles = makeStyles(() => ({
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+  carouselItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    cursror: "pointer",
+    textTransform: "uppercase",
+    color: "white",
+  },
+}));
+
+const responsiveItems = {
+  0: { items: 2 },
+  512: { items: 4 },
+};
+
+export const numberWithCommas = (x: number) =>
+  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const [trending, setTrending] = useState<any>([]);
+  const { currency, symbol } = CryptoState();
+
+  const fetchTrendingCoins = async (): Promise<any> => {
+    const { data } = await axios.get(TrendingCoins(currency));
+    setTrending(data);
+    console.log("Fetching Trending Coins...");
+  };
+  useEffect(() => {
+    fetchTrendingCoins();
+  }, [currency]);
+
+  const classes = useStyles();
+  const items = trending.map((coin: any) => {
+    let profit = coin.price_change_percentage_24h > 0;
+    return (
+      <Link to={`/coins/${coin.id}`} className={classes.carouselItem}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>{coin?.symbol} &nbsp;</span>
+        <span>
+          {profit && "+"} {coin?.price_change_percentage_24h}
+        </span>
+        <span>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
+
+  return (
+    <div className={classes.carousel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsiveItems}
+        autoPlay
+        items={items}
+      />
+    </div>
+  );
 };
 
 export default Carousel;
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 export const COINS_API_DOMAIN = "https://api.coingecko.com/api/v3";
 
-export const CoinsList = (currency: string) =>
+export const CoinsList = (currency: string | unknown) =>
   `${COINS_API_DOMAIN}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
 export const SingleCoin = (id: number) => `${COINS_API_DOMAIN}/coins/${id}`;
@@ -8,9 +8,9 @@ export const SingleCoin = (id: number) => `${COINS_API_DOMAIN}/coins/${id}`;
 export const HistoricalChart = (
   id: number,
   days: number = 365,
-  currency: string
+  currency: string | unknown
 ) =>
   `${COINS_API_DOMAIN}/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
-export const TrendingCoins = (currency: string) =>
+export const TrendingCoins = (currency: string | unknown) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
